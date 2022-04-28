3437a2fb94f9d8b060f63cdd19c8a9420e0263b9
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-04-28 16:33:52 +0900

    [Gold IV] Title: 여행 가자, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -0,0 +1,33 @@
+import sys
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x < y :
+        represent[y] = x
+    else:
+        represent[x] = y
+
+def find(x):
+    if x != represent[x]:
+        represent[x] = find(represent[x])
+    return represent[x]
+
+N = int(input())
+M = int(input())
+represent = [_ for _ in range(N)]
+
+for i in range(N):
+    temp = list(map(int, input().split()))
+    for j in range(N):
+        if temp[j]:
+            union(i, j)
+
+journey = tuple(map(int, input().split()))
+start = represent[journey[0]-1]
+for idx in range(1, M):
+    if represent[journey[idx]-1] != start:
+        print('NO')
+        break
+else:
+    print('YES')
