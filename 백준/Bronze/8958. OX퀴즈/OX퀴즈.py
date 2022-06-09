4ff0521c7a34454f8077ce0097fc136a176971fa
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-06-09 22:23:15 +0900

    [Bronze II] Title: OX퀴즈, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,13 @@
+T = int(input())
+for i in range(T):
+    score = 0
+    tmp = 0
+    quiz = input()
+    for char in quiz:
+        if char == 'O':
+            tmp += 1
+            score += tmp
+        else:
+            tmp = 0
+
+    print(score)
