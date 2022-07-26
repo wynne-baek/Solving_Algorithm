206fa93f0a487e0f5371a2f0bb82c7f025a70aa5
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-07-26 21:19:11 +0900

    [Gold V] Title: A와 B, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,14 @@
+import sys
+
+S = list(input())
+T = list(input())
+
+while len(S) != len(T):
+    char = T.pop()
+    if char == 'B':
+        T.reverse()
+
+if S == T:
+    print(1)
+else:
+    print(0)
